371cb3faa49a5784eb3c34cab6c3d528621ec995
naoki-takashio <nayuta_1125@yahoo.co.jp>
2015-09-27 13:08:09 +0900

    並べ替えjson受け取り用に変更

--- src/main/java/com/example/rakus_schedule/web/TaskController.java
+++ src/main/java/com/example/rakus_schedule/web/TaskController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -85,8 +87,8 @@ public class TaskController {
 	 * @param form 入力パラメータ
 	 * @param model 一覧画面表示
 	 */
-	@RequestMapping(value = "orderupdate")
-	public String orderupdate(@Validated OrderTaskForm form, Model model) {
+	@RequestMapping(value = "orderupdate", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String orderupdate(@Validated @RequestBody OrderTaskForm form, Model model) {
 		OrderTask orderTask = new OrderTask();
 		BeanUtils.copyProperties(form, orderTask);
 		//並べ替えを行う
